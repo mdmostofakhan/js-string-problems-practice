fcf0e7cfd1ebc42af8da2666a47750bdce801636
mostofa khan <mdmostofakhan1972@gmail.com>
2023-02-06 14:49:43 +0600

    practice

--- fibo.js
+++ fibo.js
@@ -1,1 +1,30 @@
-// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+// const fibo = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+/*
+fibo[3] = fibo[2] + fibo[1];
+fibo[4] = fibo[3] + [2];
+fibo[5] = fibo[4] + [3];
+fibo[6] = fibo[5] + [4];
+fibo[60] = fibo[59] + [58];
+fibo[n] = fibo[n-1] + [n-2];
+fibo[i] = fibo[i-1] + [i-2]
+*/
+
+// const fibo = [0, 1];
+// for (let i = 2; i <= 15; i++){
+//     fibo[i] = fibo[i - 1] + fibo[i - 2];
+// }
+// console.log(fibo);
+
+
+
+
+const fibo = [0, 1];
+for (let i = 2; i <= 20; i++){
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+}
+// console.log(fibo);
+
+for (let i = 0; i < 10; i++){
+    const random = Math.round(Math.random()*10);
+    console.log(random);
+}
--- practice.js
+++ practice.js
@@ -1,15 +1,25 @@
-function lowSmall(numbers) {
-    let largest = numbers[0];
-    for (let i = 0; i < numbers.leaght; i++){
-        const index = i;
-        const elements = numbers[index];
-        if (elements < largest) {
-            largest = elements
-        }
-    }
-    return largest;
-}
+// function lowSmall(numbers) {
+//     let largest = numbers[0];
+//     for (let i = 0; i < numbers.leaght; i++){
+//         const index = i;
+//         const elements = numbers[index];
+//         if (elements < largest) {
+//             largest = elements
+//         }
+//     }
+//     return largest;
+// }
 
 const heights = [55, 145, 235, 23, 100]
-const tallest = lowSmall(heights);
-console.log('small numr', tallest);
+// const tallest = lowSmall(heights);
+// console.log('small numr', tallest);
+
+const number = 13.67; 
+const answer = Math.floor(number); 
+// console.log(answer);
+
+function add(a, b){
+    return a + b;
+  }
+  console.log(add("adam" + "eve"))
+  
